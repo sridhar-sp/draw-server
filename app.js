22d861bff9034b86b21d163dae2a928fad38c0ad
sridhar.subramani <>
2020-09-19 18:13:33 +0530

    Use port from env for socket io server

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const SoketServer = require('socket.io')
 
-// const PORT = 3000;
+const FALLBACK_PORT = 3000;
 
-// const server = SoketServer(PORT)
+const server = SoketServer(process.env.PORT || FALLBACK_PORT)
 
-// server.on("connection",socket=>{
-//     console.log("Socket connected ${socket.id}")
-// })
+server.on("connection",socket=>{
+    console.log("Socket connected ${socket.id}")
+})
 
-var http = require('http');
+// var http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write(`Welcome to draw and guess server ${process.env.PORT}`); //write a response to the client
-  res.end(); //end the response
-}).listen(process.env.PORT || 8080); //the server object listens on port 8080
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write(`Welcome to draw and guess server ${process.env.PORT}`); //write a response to the client
+//   res.end(); //end the response
+// }).listen(process.env.PORT || 8080); //the server object listens on port 8080
 
 console.log(`ENV PORT ${process.env.PORT}`)
