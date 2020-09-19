59b09066ffb0c9cdd0953e582e64cfe2f5af07f4
sridhar.subramani <>
2020-09-19 17:48:45 +0530

    Add simple http server

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const SoketServer = require('socket.io')
 
-const PORT = 3000;
+// const PORT = 3000;
 
-const server = SoketServer(PORT)
+// const server = SoketServer(PORT)
 
-server.on("connection",socket=>{
-    console.log("Socket connected ${socket.id}")
-})
+// server.on("connection",socket=>{
+//     console.log("Socket connected ${socket.id}")
+// })
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Welcome to draw and guess server'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
