de6ea11bdbf3702955792fdcbfff8ea0255017d6
sridhar.subramani <>
2020-09-22 21:50:29 +0530

    Add delay to verify thread usage in nodejs at heroku

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
+
+const express = require('express')
 const SoketServer = require('socket.io')
+
+const app = express()
 
 const FALLBACK_PORT = 3000;
 
-const server = SoketServer(process.env.PORT || FALLBACK_PORT)
+const PORT = process.env.PORT || FALLBACK_PORT;
 
-server.on("connection",socket=>{
-    console.log("Socket connected ${socket.id}")
+const httpServer = app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+});
+
+const server = SoketServer(httpServer)
+
+server.on("connection", socket => {
+    console.log(`Socket connected ${socket.id}`)
+
+    socket.on("global_channel", data => {
+        console.log(`global_channel ${data}`)
+
+        if (data.indexOf('1') != -1) {
+            socket.broadcast.emit("global_channel", `broadcasting from ${socket.id} msg is ${data}`)
+        } else if (data.indexOf('2') != -1) {
+            socket.emit("global_channel", "Sending data to onlt the specific socket")
+        } else {
+            server.emit("global_channel", `Server sending to all ${data}`)
+        }
+
+    })
 })
 
-// var http = require('http');
+app.get('/', (req, res) => {
+    res.send("Welcome")
 
-// //create a server object:
-// http.createServer(function (req, res) {
-//   res.write(`Welcome to draw and guess server ${process.env.PORT}`); //write a response to the client
-//   res.end(); //end the response
-// }).listen(process.env.PORT || 8080); //the server object listens on port 8080
+    for (i = 0; i < 100000; i++) {
+        //res.send(`<br> i ${i} j 0 k 0`)
+        for (j = 0; j < 1000000; j++) {
+            //res.send(`<br> i ${i} j ${j} k 0`)
+            for (k= 0; k < 1000000; k++) {
+            
+            }
+    
+        }
+    }
+    res.send(`<br> i ${i} j ${j} k ${k}`)
 
-console.log(`ENV PORT ${process.env.PORT}`)
+});
+
+
+
