9582e152598469d339c1b36c5fb28a195ce0e010
sridhar.subramani <garu_sridhar@yahoo.com>
2021-01-11 19:52:08 +0530

    Revert "Moved GamePlayInfo mutator methods to GamePlayInfoRepository"
    This reverts commit 4a5cfaf7247dd597e257f65c98dc98fdd92d8fb6.

--- src/repositories/GamePlayInfoRepository.ts
+++ src/repositories/GamePlayInfoRepository.ts
@@ -4,11 +4,8 @@ import GamePlayStatus from "../models/GamePlayStatus";
 import RedisHelper from "../redis/RedisHelperV2";
 import logger from "../logger/logger";
 import Participant from "../models/Participant";
-import Task from "../scheduler/Task";
 
 class GamePlayInfoRepository {
-  private static TAG = "GamePlayInfoRepository";
-
   private redisHelper: RedisHelper;
 
   public static create(redisHelper: RedisHelper): GamePlayInfoRepository {
@@ -52,22 +49,11 @@ class GamePlayInfoRepository {
   }
 
   addParticipant(gameKey: string, socketId: string): Promise<void> {
-    this.log(`addParticipant ${socketId} for game ${gameKey}`);
+    logger.log(`addParticipant ${socketId} for game ${gameKey}`);
     return new Promise((resolve, reject) => {
       this.getOrCreateGameInfo(gameKey)
         .then((gamePlayInfo) => {
-          const index = this.findParticipant(gamePlayInfo, socketId);
-
-          if (index != -1) {
-            this.warn(`Participant record already available at index ${index}`);
-            return false;
-          }
-
-          const participant = Participant.create(socketId);
-
-          gamePlayInfo.participants.push(participant);
-          this.log(`Added participant ${participant.socketId}`);
-
+          gamePlayInfo.addParticipant(Participant.create(socketId));
           return this.redisHelper.setString(gameKey, gamePlayInfo.toJson());
         })
         .then((_) => resolve())
@@ -76,24 +62,13 @@ class GamePlayInfoRepository {
   }
 
   removeParticipant(gameKey: string, socketId: string): Promise<void> {
-    this.log(`removeParticipant ${socketId} for game ${gameKey}`);
+    logger.log(`removeParticipant ${socketId} for game ${gameKey}`);
     return new Promise((resolve, reject) => {
       this.getGameInfo(gameKey)
         .then((gamePlayInfo) => {
           if (null == gamePlayInfo) throw new Error(`No game record found for keu: ${gameKey}`);
 
-          const index = this.findParticipant(gamePlayInfo, socketId);
-
-          if (index == -1) {
-            this.log(`No user record found for ${socketId}`);
-            return false;
-          }
-
-          const participantToRemove = gamePlayInfo.participants[index];
-          this.log(`Removed participant from game play info ${participantToRemove}`);
-
-          gamePlayInfo.participants.splice(index, 1);
-
+          gamePlayInfo.removeParticipant(socketId);
           return this.redisHelper.setString(gameKey, gamePlayInfo.toJson());
         })
         .then((_) => resolve())
@@ -102,31 +77,18 @@ class GamePlayInfoRepository {
   }
 
   updateGameStatus(gameKey: string, gamePlayStatus: GamePlayStatus): Promise<void> {
-    this.log(`updateGameStatus ${gamePlayStatus} for game ${gameKey}`);
+    logger.log(`updateGameStatus ${gamePlayStatus} for game ${gameKey}`);
     return new Promise((resolve, reject) => {
       this.getGameInfo(gameKey)
         .then((gamePlayInfo) => {
           if (null == gamePlayInfo) throw new Error(`No game record found for keu: ${gameKey}`);
 
-          gamePlayInfo.gamePlayStatus = gamePlayStatus;
+          gamePlayInfo.updateGamePlayStatus(gamePlayStatus);
           return this.redisHelper.setString(gameKey, gamePlayInfo.toJson());
         })
         .then((_) => resolve())
         .catch((err) => reject(err));
     });
-  }
-
-  findParticipant(gamePlayInfo: GamePlayInfo, socketId: string): number {
-    if (!gamePlayInfo.participants || gamePlayInfo.participants.length == 0) return -1;
-    return gamePlayInfo.participants.findIndex((participant) => participant.socketId == socketId);
-  }
-
-  private log(log: string) {
-    logger.log(`${GamePlayInfoRepository.TAG} :: ${log}`);
-  }
-
-  private warn(log: string) {
-    logger.warn(`${GamePlayInfoRepository.TAG} :: ${log}`);
   }
 }
 
