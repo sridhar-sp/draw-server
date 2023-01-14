178c2394846236925a8f41d14b156db717daa170
sridhar-sp <sridharthechosenone@gmail.com>
2023-01-14 19:04:23 +0530

    Remove temp project url

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,9 +1,8 @@
-const config = require('../../src/config/index.js')
-import admin from 'firebase-admin'
+const config = require("../../src/config/index.js");
+import admin from "firebase-admin";
 
 admin.initializeApp({
-    databaseURL: "https://draw-37e3c.firebaseio.com",
-    credential: admin.credential.cert(config.firebaseConfig)
+  credential: admin.credential.cert(config.firebaseConfig),
 });
 
-export default { auth: admin.auth, firestore: admin.firestore }
+export default { auth: admin.auth, firestore: admin.firestore };
